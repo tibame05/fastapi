3c353132cf7d5afa85ff1921cd419d1789f4fb8d
pei yu ji <abc0970870@gmail.com>
2025-07-29 21:52:55 +0800

    commit

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="test",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
-    author="tibame05",  # Optional
+    author="peiyuji",  # Optional
     author_email="abc0970870@gmail.com",  # Optional
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
